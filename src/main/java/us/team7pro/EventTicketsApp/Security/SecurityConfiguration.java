1cf79c1a852d27cb42ee22435ed6017f5485e1cc
zuolizhu <zuolizhu5960x@gmail.com>
2018-12-01 20:53:30 -0500

    user details service added

--- src/main/java/us/team7pro/EventTicketsApp/Security/SecurityConfiguration.java
+++ src/main/java/us/team7pro/EventTicketsApp/Security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package us.team7pro.EventTicketsApp.Security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -16,5 +24,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
             .and()
             .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 }
--- src/main/java/us/team7pro/EventTicketsApp/SecurityRepo/UserRepo.java
+++ src/main/java/us/team7pro/EventTicketsApp/SecurityRepo/UserRepo.java
@@ -3,5 +3,8 @@ package us.team7pro.EventTicketsApp.SecurityRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import us.team7pro.EventTicketsApp.Domain.User;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
