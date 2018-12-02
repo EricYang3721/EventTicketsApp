83077f412e48e43a5f375aae9347e2086677de92
zuolizhu <zuolizhu5960x@gmail.com>
2018-12-01 19:53:27 -0500

    security dependencies added, default config added

---
+++ src/main/java/us/team7pro/EventTicketsApp/Security/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package us.team7pro.EventTicketsApp.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+    }
+}
