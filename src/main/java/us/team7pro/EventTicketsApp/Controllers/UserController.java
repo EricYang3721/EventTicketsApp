6b9c8c461231b74fa14e773328471e43f8aa76f8
zuolizhu <zuolizhu5960x@gmail.com>
2018-12-02 19:08:52 -0500

    crazy signin page

--- src/main/java/us/team7pro/EventTicketsApp/Controllers/UserController.java
+++ src/main/java/us/team7pro/EventTicketsApp/Controllers/UserController.java
@@ -1,22 +1,14 @@
 package us.team7pro.EventTicketsApp.Controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import us.team7pro.EventTicketsApp.Models.*;
+import org.springframework.web.bind.annotation.*;
 import us.team7pro.EventTicketsApp.Models.Transaction;
-import us.team7pro.EventTicketsApp.Repositories.UserRepository;
+import us.team7pro.EventTicketsApp.Models.User;
 import us.team7pro.EventTicketsApp.Repositories.EventRepository;
 import us.team7pro.EventTicketsApp.Repositories.TransactionRepository;
-
-import java.util.ArrayList;
-import java.util.List;
+import us.team7pro.EventTicketsApp.Repositories.UserRepository;
 
 @Controller
 public class UserController {
@@ -28,28 +20,28 @@ public class UserController {
 
     @Autowired
     TransactionRepository transactionRepository;
-
+//
     @GetMapping("/organizerdashboard")
     public String organizerdashboard() {
         return "organizerdashboard";
     }
-
-    @GetMapping("/admindashboard")
-    public String admindashboard() {
-        return "admindashboard";
-    }
-
-    @GetMapping("/userdashboard")
-    public String userdashboard(Model model) {
-        List<Event> events = new ArrayList<Event>();;
-        List<Transaction> userTransactions = transactionRepository.findByUserID(0);
-        for (Transaction transaction : userTransactions) {
-            events.add(eventRepository.findByEventID(transaction.getEventID()));
-        }
-        model.addAttribute("transactions", userTransactions);
-        model.addAttribute("events", events);
-        return "userdashboard";
-    }
+//
+//    @GetMapping("/admindashboard")
+//    public String admindashboard() {
+//        return "admindashboard";
+//    }
+//
+//    @GetMapping("/userdashboard")
+//    public String userdashboard(Model model) {
+//        List<Event> events = new ArrayList<Event>();;
+//        List<Transaction> userTransactions = transactionRepository.findByUserID(0);
+//        for (Transaction transaction : userTransactions) {
+//            events.add(eventRepository.findByEventID(transaction.getEventID()));
+//        }
+//        model.addAttribute("transactions", userTransactions);
+//        model.addAttribute("events", events);
+//        return "userdashboard";
+//    }
 
     // @RequestMapping("/purchaseTicket")
     // public String purchaseTicket(@RequestParam int eventID, @RequestParam int userID) {
@@ -91,34 +83,34 @@ public class UserController {
     }
 
 
-    @GetMapping("/signin")
-    public String userSignIn(Model model){
-        model.addAttribute("visitor", new User());
-        return "signin";
-    }
+//    @GetMapping("/signin")
+//    public String userSignIn(Model model){
+//        model.addAttribute("visitor", new User());
+//        return "signin";
+//    }
+//
+//    @PostMapping("/signin")
+//    public String userSignInResult(@ModelAttribute User visitor){
+//        User dbUser = userRepository.findByEmail(visitor.getEmail());
+//        if(dbUser==null || !dbUser.getPassword().equals(visitor.getPassword())){
+//            return "errors/signinError";
+//        }  else {
+//            if(dbUser.getRole().equals("Admin")){
+//                return "admindashboard";
+//            } else if (dbUser.getRole().equals("Organizer")){
+//                return "organizerdashboard";
+//            } else {
+//                return "success/signinSuccess";
+//            }
+//
+//        }
+//
+//    }
 
-    @PostMapping("/signin")
-    public String userSignInResult(@ModelAttribute User visitor){
-        User dbUser = userRepository.findByEmail(visitor.getEmail());
-        if(dbUser==null || !dbUser.getPassword().equals(visitor.getPassword())){
-            return "errors/signinError";
-        }  else {
-            if(dbUser.getRole().equals("Admin")){
-                return "admindashboard";
-            } else if (dbUser.getRole().equals("Organizer")){
-                return "organizerdashboard";
-            } else {
-                return "success/signinSuccess";
-            }
-            
-        }
-
-    }
-
-    @GetMapping("/logout")
-    public String userLogout(){
-        return "success/logoutSuccess";
-    }
+//    @GetMapping("/logout")
+//    public String userLogout(){
+//        return "success/logoutSuccess";
+//    }
 
     
 }
